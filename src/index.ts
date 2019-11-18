4af8746f2ad8040ff18c52d09dd52271a6233f77
Francisco Galera <franciscojose@gmx.de>
2019-11-18 23:38:12 +0100

    fromEvent

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import {fromEvent} from 'rxjs';
+
+//Eventos del dom
 
 
+const src1$ = fromEvent<MouseEvent>(document, 'click');
+const src2$ = fromEvent<KeyboardEvent>(document, 'keyup');
 
+const obs = {
+    next: val => console.log('next: ', val)
+}
+
+src1$.subscribe(({x, y}) => {
+    console.log('hhh', x, y)
+});
+src2$.subscribe(val => console.log('jjj', val.key));
 
 
 
